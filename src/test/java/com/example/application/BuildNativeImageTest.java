0b3701d63a710965c551d9d3e74def862da06ae3
dashaun <dashaun@dashaun.com>
2023-03-09 15:15:48 -0600

    spring-boot:build-image tests

---
+++ src/test/java/com/example/application/BuildNativeImageTest.java
@@ -0,0 +1,60 @@
+package com.example.application;
+
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.MavenInvocationException;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.LazyFuture;
+
+import java.io.File;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+public class BuildNativeImageTest {
+
+	private static final Future<String> IMAGE_FUTURE = new LazyFuture<>() {
+		@Override
+		protected String resolve() {
+			// Find project's root dir
+			File cwd;
+			cwd = new File(".");
+			while (!new File(cwd, "mvnw").isFile()) {
+				cwd = cwd.getParentFile();
+			}
+
+			var properties = new Properties();
+			properties.put("skipTests", "true"); // Don't want to create a loop
+
+			var request = new DefaultInvocationRequest().setPomFile(new File(cwd, "pom.xml"))
+					.setGoals(List.of("spring-boot:build-image")).setMavenExecutable(new File(cwd, "mvnw"))
+					.setProfiles(List.of("production", "build-native-image-test", "native")).setProperties(properties);
+
+			InvocationResult buildResult;
+			try {
+				buildResult = new DefaultInvoker().execute(request);
+			}
+			catch (MavenInvocationException e) {
+				throw new RuntimeException(e);
+			}
+
+			if (buildResult.getExitCode() != 0) {
+				throw new RuntimeException(buildResult.getExecutionException());
+			}
+
+			return "docker.io/dashaun/hilla-aot-demo:integration-native";
+		}
+	};
+
+	@Container
+	static final GenericContainer<?> APP = new GenericContainer<>(IMAGE_FUTURE).withExposedPorts(8080);
+
+	@Test
+	void containerStartupTest() {
+		APP.start();
+	}
+
+}
